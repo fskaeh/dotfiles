e5fa3d672d4f2fc9f1e6aa245d1a8fc098d266c3
fskaeh <fopnai@posteo.net>
2022-07-11 13:50:45 +0200

    Added iTerm2 settings

--- setup_macos.zsh
+++ setup_macos.zsh
@@ -1,6 +1,8 @@
 #!/bin/zsh
+cd "${0%/*}"
 
 # Copy config files
 cp -rf ./nvim ~/.config/
 cp -f ./starship/starship.toml ~/.config/
 cp -f ./zsh/.zshrc ~/
+defaults import com.googlecode.iterm2 ./iterm2/com.googlecode.iterm2.plist
