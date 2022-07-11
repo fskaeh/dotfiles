dc9f26f41033c1e54ef0c36ba5c99b4e8931e769
fskaeh <fopnai@posteo.net>
2022-07-11 21:18:37 +0200

    Added eslint LSP config

---
+++ lua/options.lua
@@ -0,0 +1,19 @@
+vim.opt.background = 'light'
+vim.opt.breakindent = true
+vim.opt.clipboard = vim.opt.clipboard + 'unnamedplus'
+vim.opt.completeopt = 'menuone,noselect'
+vim.opt.expandtab = true
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.rnu = true
+vim.opt.shiftwidth = 2
+vim.opt.signcolumn = 'yes'
+vim.opt.smartcase = true
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.updatetime = 250
+vim.opt.wrap = true
+
