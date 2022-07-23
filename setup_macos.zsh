ad338bc732bc6886aec91790ff6f03d0e1320168
Nicolas Stadler <carniu@MacBook-Pro-de-Nicolas.local>
2022-07-23 17:19:41 +0200

    Remove SKHD and Yabai

--- setup_macos.zsh
+++ setup_macos.zsh
@@ -3,8 +3,6 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 # Copy config files
 cp -rf ./nvim ~/.config/
-cp -rf ./skhd ~/.config/
-cp -rf ./yabai ~/.config/
 cp -f ./starship/starship.toml ~/.config/
 cp -f ./zsh/.zshrc ~/
 defaults import com.googlecode.iterm2 ./iterm2/com.googlecode.iterm2.plist
