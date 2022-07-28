c9fd8ecebc496f05e4134535528ae99145e08b57
fskaeh <fopnai@posteo.net>
2022-07-28 18:28:18 +0200

    Added Yabai, SKHD and Ubersicht w/ Simple Bar

--- setup_macos.zsh
+++ setup_macos.zsh
@@ -3,6 +3,9 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 # Copy config files
 cp -rf ./nvim ~/.config/
+cp -f ./ubersicht ~/.config/
+cp -f ./skhd ~/.config/
 cp -f ./starship/starship.toml ~/.config/
+cp -f ./yabai ~/.config/
 cp -f ./zsh/.zshrc ~/
 defaults import com.googlecode.iterm2 ./iterm2/com.googlecode.iterm2.plist
