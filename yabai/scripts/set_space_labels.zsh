2e1b0384ad445ab0362c7b4905575b39ea64c84d
fskaeh <fopnai@posteo.net>
2022-07-29 21:14:15 +0200

    Cleaned up some config files

---
+++ yabai/scripts/set_space_labels.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+yabai -m space 1 --label "1 www"
+yabai -m space 2 --label "2 msg"
+yabai -m space 3 --label "3 dev"
+yabai -m space 4 --label "4 org"
+yabai -m space 5 --label "5 fun"
+yabai -m space 6 --label "6 utl"
+yabai -m space 7 --label "7 oth"
+yabai -m space 8 --label "8 oth"
+yabai -m space 9 --label "9 oth"
