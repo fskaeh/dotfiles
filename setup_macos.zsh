9ff2ed4f6242ce2a1b0a6526b56d5833572642d7
fskaeh <fopnai@posteo.net>
2022-07-30 12:05:00 +0200

    Added tmux

--- setup_macos.zsh
+++ setup_macos.zsh
@@ -7,6 +7,7 @@ cp -f ./nvim ~/.config/
 cp -f ./skhd ~/.config/
 cp -f ./spacebar ~/.config/
 cp -f ./starship/starship.toml ~/.config/
+cp -f ./tmux/.tmux.conf ~/
 cp -f ./yabai ~/.config/
 cp -f ./zsh/.zshrc ~/
 defaults import com.googlecode.iterm2 ./iterm2/com.googlecode.iterm2.plist
