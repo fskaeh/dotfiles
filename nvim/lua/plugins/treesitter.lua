964c91c14ec28a8e0faa272a43e01eabbf280176
fskaeh <fopnai@posteo.net>
2022-07-31 23:31:24 +0200

    Added Ocaml

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'lua', 'typescript', 'javascript', 'html', 'css', 'json', 'scss', 'rust', 'go', 'python' },
+  ensure_installed = { 'lua', 'typescript', 'javascript', 'html', 'css', 'json', 'scss', 'rust', 'go', 'python', 'ocaml' },
 
   highlight = { enable = true },
   indent = { enable = true },
