eb8a3218eb04278d03bcf03392d54fe75ea09e20
fskaeh <fopnai@posteo.net>
2022-07-31 10:38:26 +0200

    Improved lualine config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,15 +1,18 @@
 -- [[ Lualine ]]
 require('lualine').setup({
   options = {
-    icons_enabled = false,
+    icons_enabled = true,
     theme = 'onelight',
     component_separators = '|',
     section_separators = '',
   },
   sections = {
-		lualine_c = {
-			'lsp_progress'
-		}
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename', 'lsp_progress'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'location'},
+    lualine_z = {'progress'}
 	}
 })
 
